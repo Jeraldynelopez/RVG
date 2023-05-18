3fd68c2aff4b16ba7ca2d41dbb0bd8217d922ac9
Jeraldynelopez <132521041+Jeraldynelopez@users.noreply.github.com>
2023-05-17 22:08:06 -0500

    Quinta entrega

--- fronted/grupo3/titulo3.py
+++ fronted/grupo3/titulo3.py
@@ -1,8 +1,21 @@
+import dash
 from dash import html
 import dash_bootstrap_components as dbc
+from PIL import Image
+
+
+ruta_imagen_seco = Image.open('imagenes\seco.png')
+ruta_imagen_saturado = Image.open('imagenes\saturado.png')
+ruta_imagen_humedo = Image.open('imagenes\humedo.png')
 
 titulo3 = dbc.Container(
     [
         html.H1('Diagrama de fases del suelo'),
+        html.H2('Diagrama suelo seco'),
+        html.Img(src=ruta_imagen_seco, style={'max-width': '100%'}),
+        html.H3('Diagrama suelo saturado'),
+        html.Img(src=ruta_imagen_saturado, style={'max-width': '100%'}),
+        html.H4('Diagrama suelo humedo'),
+        html.Img(src=ruta_imagen_humedo, style={'max-width': '100%'}),
     ]
 )
--- fronted/main.py
+++ fronted/main.py
@@ -1,22 +1,28 @@
+import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 
-#import fronted
+# Importar componentes de título
 from fronted.grupo1.titulo1 import titulo1
 from fronted.grupo2.titulo2 import titulo2
 from fronted.grupo3.titulo3 import titulo3
 
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 layout = dbc.Container(
     [
         dbc.Row(
             [
-                dbc.Col(titulo1, md=12, style={'background-color':'white'}),
-                dbc.Col(titulo2, md=8, style={'background-color':'blue'}),
-                dbc.Col(titulo3, md=4, style={'background-color':'gray'}),
-
+                dbc.Col(titulo1, md=12, style={'background-color': 'rgba(255, 255, 255, 0.5)'}),
+                dbc.Col(titulo2, md=8, style={'background-color': 'rgba(0, 0, 255, 0.5)'}),
+                dbc.Col(titulo3, md=4, style={'background-color': 'rgba(128, 128, 128, 0.5)'}),
             ]
         )
     ]
 )
+
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
